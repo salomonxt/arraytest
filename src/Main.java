c94031d81cf185bdfcfee988a74bffd253a3f943
salomonxt <salomonxt@protonmail.ch>
2025-08-22 15:13:19 +0200

    add

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome, nun gehts um ARRAYs!");
+        System.out.println("Hello and welcome, nun gehts um ARRAYs!");
 
         }
     }
