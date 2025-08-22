94dbbb61e3c226e8c59daecf5de106c38f9d0d27
salomonxt <salomonxt@protonmail.ch>
2025-08-22 15:31:13 +0200

    add

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hallo and welcome, nun gehts um ARRAYs!");
         System.out.println("=======================================");
-        System.out.println("");
+        System.out.println();
         String[] autos = {"Volvo", "BMW", "Ford", "Mazda"};
         System.out.println(autos[1]);
         autos[1]="Salomon";
